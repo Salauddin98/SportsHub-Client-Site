388c2fbb246af6dd7333ee4e53391403547fbb8e
Salauddin98 <webdevelopment20236@gmail.com>
2023-06-13 20:09:25 +0600

    added some items

--- src/page/Home/Home/Home.jsx
+++ src/page/Home/Home/Home.jsx
@@ -4,12 +4,7 @@ const Home = () => {
   return (
     <div>
       <Banner></Banner>
-      <h1>kire</h1>
-      <p>ar balo lage nh</p>
-      <p>scic</p>
-      <p>commit</p>
-      <p>hi</p>
-      <p>salauddin</p>
+      <p>Home Page</p>
     </div>
   );
 };
